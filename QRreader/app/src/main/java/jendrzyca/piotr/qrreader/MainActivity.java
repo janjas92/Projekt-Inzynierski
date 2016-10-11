0769502210854fc553e23957320e7ca2212a3935
piotr.jendrzyca@euvic.pl <piotr.jendrzyca@euvic.pl>
2016-10-11 20:49:03 +0200

    anroid project: qrreader initail commit; .gitgnore updated

---
+++ QRreader/app/src/main/java/jendrzyca/piotr/qrreader/MainActivity.java
@@ -0,0 +1,13 @@
+package jendrzyca.piotr.qrreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
