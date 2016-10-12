d94a6caa8ce69b6139732a1371b123232bebac11
pjendrzyca <piotr.jendrzyca@euvic.pl>
2016-10-12 22:14:46 +0200

    fragments added

--- QRreader/app/src/main/java/jendrzyca/piotr/qrreader/MainActivity.java
+++ QRreader/app/src/main/java/jendrzyca/piotr/qrreader/MainActivity.java
@@ -1,13 +1,55 @@
 package jendrzyca.piotr.qrreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    //@BindView(R.id.button)
+    Button scannerButton;
+
+
+//    @OnClick(R.id.button)
+//    void startScanner(View view) {
+//        Toast.makeText(this, "click", Toast.LENGTH_LONG);
+//
+//        new IntentIntegrator(this).initiateScan();
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("error","");
+            } else {
+                Log.d("scanned: ",result.getContents());
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
