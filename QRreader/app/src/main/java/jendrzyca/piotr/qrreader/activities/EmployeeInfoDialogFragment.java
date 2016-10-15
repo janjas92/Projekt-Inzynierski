0c4e9140c033bb128d80085465d762ea4cd27cf6
huddy14 <huddy14@gmail.com>
2016-10-15 17:45:40 +0200

    ui changed; scanner now works for full screen

--- QRreader/app/src/main/java/jendrzyca/piotr/qrreader/activities/EmployeeInfoDialogFragment.java
+++ QRreader/app/src/main/java/jendrzyca/piotr/qrreader/activities/EmployeeInfoDialogFragment.java
@@ -3,15 +3,35 @@ package jendrzyca.piotr.qrreader.activities;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jendrzyca.piotr.qrreader.R;
 
 /**
  * Created by huddy on 14.10.2016.
  */
 
 public class EmployeeInfoDialogFragment extends DialogFragment {
+
+    @BindView(R.id.tbFirstName)TextView firstName;
+    @BindView(R.id.tbLastName)TextView lastName;
+    @BindView(R.id.tbMessage)TextView welcomeMessage;
+    @BindView(R.id.avatar)ImageView avatar;
+
+    String message;
 
     private ClosingCallback listener;
 
@@ -27,12 +47,28 @@ public class EmployeeInfoDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        String message = getArguments().getString("hashCode");
-        return new AlertDialog.Builder(getActivity())
-                .setTitle("Leaving launcher")
-                .setMessage(message)
-                .setPositiveButton("x", (dialogInterface, i) -> dialogInterface.dismiss())
-                .create();
+//        String message = getArguments().getString("hashCode");
+//        //firstName.setText(message);
+//        LayoutInflater inflater = getActivity().getLayoutInflater();
+//
+//        AlertDialog dialog = new AlertDialog.Builder(getActivity())
+//                .setView(R.layout.dialog_layout)
+//                .setPositiveButton("x", (dialogInterface, i) -> dialogInterface.dismiss())
+//                .create();
+//        return dialog;
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        message = getArguments().getString("hashCode");
+        dialog.requestWindowFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        return dialog;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_layout, container, false);
+        ButterKnife.bind(this, view);
+        firstName.setText(message);
+        return view;
     }
 
     public static EmployeeInfoDialogFragment newInstance(String code, ClosingCallback listener) {
--- QRreader/app/src/main/java/jendrzyca/piotr/qrreader/activities/MainActivity.java
+++ QRreader/app/src/main/java/jendrzyca/piotr/qrreader/activities/MainActivity.java
@@ -1,17 +1,29 @@
 package jendrzyca.piotr.qrreader.activities;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.graphics.Rect;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.ViewfinderView;
+import com.journeyapps.barcodescanner.camera.CameraManager;
+import com.journeyapps.barcodescanner.camera.CameraSettings;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.BindView;
@@ -22,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements EmployeeInfoDialo
 
     @BindView(R.id.scanner)
     DecoratedBarcodeView scanner;
+    @BindView(R.id.tvDate)TextView tvDate;
+    @BindView(R.id.tvTime)TextView tvTime;
 
     BarcodeCallback callback = new BarcodeCallback() {
         @Override
@@ -34,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements EmployeeInfoDialo
 
             // Hide after some seconds
             dialog.show(fragmentManager, "test");
+            Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_LONG).show();
         }
 
         @Override
@@ -47,18 +62,43 @@ public class MainActivity extends AppCompatActivity implements EmployeeInfoDialo
         scanner.resume();
     }
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        setDateAndTime();
+
+
+        //setting the scanner to use front camera
+        CameraSettings settings = scanner.getBarcodeView().getCameraSettings();
+        //CameraManager cameraManager = scanner.getViewFinder();
+        settings.setRequestedCameraId(Camera.CameraInfo.CAMERA_FACING_FRONT);
+
+        //enabling new settings
+        scanner.getBarcodeView().setCameraSettings(settings);
+        //hiding decorations
+        scanner.getViewFinder().setVisibility(View.INVISIBLE);
         //clearing scanners display name
         scanner.setStatusText("");
         //setting scanner to run continuously
         scanner.decodeContinuous(callback);
     }
 
+    private void setDateAndTime() {
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String date = dateFormat.format(calendar.getTime());
+        date = date.replace(date.charAt(0), date.toUpperCase().charAt(0));
+        String time = timeFormat.format(calendar.getTime());
+
+        tvDate.setText(date);
+        tvTime.setText(time);
+    }
 
     @Override
     protected void onResume() {
